2e8a6bd7a877e3af88c294d7af8da112dd5d1af1
Yu-Cheng, Chang <45489725+Yucheng0208@users.noreply.github.com>
2025-05-02 11:18:39 +0800

    Update PSNR.py

--- 111-2/Introduction_to_Digital_Image_Processing/PSNR.py
+++ 111-2/Introduction_to_Digital_Image_Processing/PSNR.py
@@ -1,13 +1,10 @@
 import tensorflow as tf
 
-
 def read_img(path):
     return tf.image.decode_image(tf.read_file(path))
 
-
 def psnr(tf_img1, tf_img2):
     return tf.image.psnr(tf_img1, tf_img2, max_val=255)
-
 
 def _main():
     t1 = read_img('t1.jpg')
@@ -16,7 +13,6 @@ def _main():
         sess.run(tf.global_variables_initializer())
         y = sess.run(psnr(t1, t2))
         print(y)
-
 
 if __name__ == '__main__':
     _main()
